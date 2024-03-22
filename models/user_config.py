31b0c5bdda3e9bb188b8ed249bf5b9dabef34c1e
yilun_jin <jyl.jal123@gmail.com>
2024-03-22 05:08:06 +0000

    Update models/user_config.py

--- models/user_config.py
+++ models/user_config.py
@@ -1,11 +1,12 @@
 # Importing DummyModel from the models package.
 # The DummyModel class is located in the dummy_model.py file inside the 'models' directory.
-from models.dummy_model import DummyModel
+from models.dummy_model import DummyModel, Vicuna2ZeroShot
 
 # This line establishes an alias for the DummyModel class to be used within this script.
 # Instead of directly using DummyModel everywhere in the code, we're assigning it to 'UserModel'.
 # This approach allows for easier reference to your model class when evaluating your models,
 UserModel = DummyModel
+
 
 # When implementing your own model please follow this pattern:
 #
@@ -17,3 +18,6 @@ UserModel = DummyModel
 # Finally, assign YourModel to UserModel as shown below to use it throughout your script.
 #
 # UserModel = YourModel
+
+# For example, you can try the Vicuna2ZeroShot baseline by uncommenting the following line. 
+# UserModel = Vicuna2ZeroShot
